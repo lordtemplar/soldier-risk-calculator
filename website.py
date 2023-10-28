490113fb055f469224cdb014c0b20de27fce89c1
lordtemplar <lordtemplar0@gmail.com>
2023-10-29 02:29:20 +0700

    Update website.py

--- website.py
+++ website.py
@@ -1,14 +1,22 @@
 import streamlit as st
 
 def main():
-    st.title("ID No. Input Interface")
+    st.title("Health Data Input Interface")
 
-    # Input textbox for ID No.
-    id_no = st.text_input("Enter ID No.")
+    # Input fields
+    soldier_id = st.text_input("Enter Soldier ID")
+    weight = st.number_input("Enter Weight (kg)", min_value=0.0)
+    body_temp = st.number_input("Enter Body Temperature (°C)", min_value=0.0)
+    body_water = st.number_input("Enter Body Water (%)", min_value=0.0)
+    urine_color = st.selectbox("Select Urine Color", ["Clear", "Light Yellow", "Yellow", "Dark Yellow", "Amber", "Brown"])
 
-    # Submit button
-    if st.button("Submit"):
-        st.write(f"You have submitted ID No.: {id_no}")
+    # Calculate button
+    if st.button("Calculate"):
+        st.write(f"Soldier ID: {soldier_id}")
+        st.write(f"Weight: {weight} kg")
+        st.write(f"Body Temperature: {body_temp} °C")
+        st.write(f"Body Water: {body_water} %")
+        st.write(f"Urine Color: {urine_color}")
 
 if __name__ == "__main__":
     main()
