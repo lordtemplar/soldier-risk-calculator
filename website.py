56f855cca1f25610278e6805a1d006d3c3624d97
lordtemplar <lordtemplar0@gmail.com>
2023-10-29 12:49:00 +0700

    Update website.py

--- website.py
+++ website.py
@@ -14,20 +14,18 @@ st.title("Google Sheets Data Fetcher by Soldier ID")
 SHEET_URL = "https://docs.google.com/spreadsheets/d/1sTGeISgyGZgngAkBl86cPcdIzfYKFyotXQUhcslGilw/edit#gid=1050462434"
 
 # Input for Soldier ID
+soldier_id = st.text_input("Enter Soldier_ID:")
 
+# Fetch and display data using the specified structure
 if st.button("Fetch Data"):
     spreadsheet = client.open_by_url(SHEET_URL)
     worksheet = spreadsheet.sheet1
     
     # Get all records
     records = worksheet.get_all_records()
-
-    # Debug: Display all Soldier IDs
-    all_soldier_ids = [str(record.get("Soldier ID", "N/A")).strip() for record in records]
-    st.write("All Soldier IDs:", all_soldier_ids)
     
-    # Filter records by Soldier ID
-    matching_records = [record for record in records if str(record.get("Soldier ID")).strip() == soldier_id.strip()]
+    # Filter records by Soldier_ID
+    matching_records = [record for record in records if record.get("Soldier_ID") == soldier_id]
     
     if matching_records:
         for record in matching_records:
@@ -35,13 +33,15 @@ if st.button("Fetch Data"):
             name = record.get("Name", "N/A")
             surname = record.get("Surname", "N/A")
             height = record.get("Height", "N/A")
+            weight = record.get("Weight", "N/A")
             
             # Display the data in Streamlit
             st.write(f"Timestamp: {timestamp}")
-            st.write(f"Soldier ID: {soldier_id}")
+            st.write(f"Soldier_ID: {soldier_id}")
             st.write(f"Name: {name}")
             st.write(f"Surname: {surname}")
             st.write(f"Height: {height}")
+            st.write(f"Weight: {weight}")
             st.write("---")  # Separator
     else:
-        st.warning(f"No data found for Soldier ID: {soldier_id}")
+        st.warning(f"No data found for Soldier_ID: {soldier_id}")
