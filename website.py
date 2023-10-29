e314c443eb3ad0ab80851e97788cbfe00dd30fec
lordtemplar <lordtemplar0@gmail.com>
2023-10-29 12:13:28 +0700

    Update website.py

--- website.py
+++ website.py
@@ -1,25 +1,38 @@
 import streamlit as st
 import gspread
-from gspread_dataframe import get_as_dataframe
+from oauth2client.service_account import ServiceAccountCredentials
 
-def get_first_row_from_sheet():
-    # Use gspread to authenticate anonymously
-    gc = gspread.client.Client()
+# Setup gspread
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("/soldier-risk-calculator-93be17dccbd3.json", scope)
+client = gspread.authorize(creds)
 
-    # Open the Google Sheet using its URL
-    sheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/1sTGeISgyGZgngAkBl86cPcdIzfYKFyotXQUhcslGilw/edit?usp=drive_link").sheet1
+# Streamlit UI
+st.title("Google Sheets CRUD App")
 
-    # Get the first row of data using gspread_dataframe
-    df = get_as_dataframe(sheet, header=None, usecols=[0, 1, 2], nrows=1)  # Adjust usecols as needed
-    return df.iloc[0].tolist()
+sheet_url = st.text_input("https://docs.google.com/spreadsheets/d/1sTGeISgyGZgngAkBl86cPcdIzfYKFyotXQUhcslGilw/edit?usp=sharing", "")
+spreadsheet = client.open_by_url(sheet_url)
 
-def main():
-    st.title("Streamlit and Google Sheets Connection Test")
+# Read
+if st.button("Read Data"):
+    worksheet = spreadsheet.sheet1
+    data = worksheet.get_all_records()
+    st.write(data)
 
-    # Button to fetch data
-    if st.button("Fetch First Row from Google Sheets"):
-        data = get_first_row_from_sheet()
-        st.write(data)
+# Write
+new_data = st.text_input("Enter new data (comma separated)", "")
+if st.button("Write Data"):
+    new_row = new_data.split(",")
+    worksheet.append_row(new_row)
 
-if __name__ == "__main__":
-    main()
+# Update
+row_number = st.number_input("Enter the row number to update", min_value=1, value=1)
+updated_data = st.text_input("Enter updated data (comma separated)", "")
+if st.button("Update Data"):
+    new_row = updated_data.split(",")
+    worksheet.update(f"A{row_number}", [new_row])
+
+# Delete
+row_number_to_delete = st.number_input("Enter the row number to delete", min_value=1, value=1)
+if st.button("Delete Data"):
+    worksheet.delete_rows(row_number_to_delete)
