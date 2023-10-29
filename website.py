46ff25c80fe9acc55f446af9aa6c73d4fa5e5897
lordtemplar <lordtemplar0@gmail.com>
2023-10-29 12:03:12 +0700

    Update website.py

--- website.py
+++ website.py
@@ -1,15 +1,17 @@
 import streamlit as st
 import gspread
+from gspread_dataframe import get_as_dataframe
 
 def get_first_row_from_sheet():
-    # Use gspread to authenticate using your personal credentials
-    gc = gspread.service_account(filename=None)  # If you have no service account, it will use your personal credentials
+    # Use gspread to authenticate anonymously
+    gc = gspread.client.Client()
 
-    # Open the Google Sheet using its ID
-    sheet = gc.open_by_key("1050462434").sheet1
+    # Open the Google Sheet using its URL
+    sheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/1sTGeISgyGZgngAkBl86cPcdIzfYKFyotXQUhcslGilw/edit?usp=drive_link").sheet1
 
-    # Get the first row of data
-    return sheet.row_values(1)
+    # Get the first row of data using gspread_dataframe
+    df = get_as_dataframe(sheet, header=None, usecols=[0, 1, 2], nrows=1)  # Adjust usecols as needed
+    return df.iloc[0].tolist()
 
 def main():
     st.title("Streamlit and Google Sheets Connection Test")
