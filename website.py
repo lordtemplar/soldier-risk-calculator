7f4f212555e5732ca055299f12be7c5b29a12b45
lordtemplar <lordtemplar0@gmail.com>
2023-10-29 12:36:23 +0700

    Update website.py

--- website.py
+++ website.py
@@ -13,9 +13,25 @@ st.title("Google Sheets Data Fetcher")
 # Fixed Google Sheet URL
 SHEET_URL = "https://docs.google.com/spreadsheets/d/1sTGeISgyGZgngAkBl86cPcdIzfYKFyotXQUhcslGilw/edit#gid=1050462434"
 
-# Fetch and display the first row
-if st.button("Fetch First Row"):
+# Fetch and display data using the specified structure
+if st.button("Fetch Data"):
     spreadsheet = client.open_by_url(SHEET_URL)
     worksheet = spreadsheet.sheet1
-    first_row = worksheet.row_values(1)
-    st.write(first_row)
+    
+    # Get all records
+    records = worksheet.get_all_records()
+    
+    for record in records:
+        timestamp = record.get("Timestamp", "N/A")
+        soldier_id = record.get("Soldier ID", "N/A")
+        name = record.get("Name", "N/A")
+        surname = record.get("Surname", "N/A")
+        height = record.get("Height", "N/A")
+        
+        # Display the data in Streamlit
+        st.write(f"Timestamp: {timestamp}")
+        st.write(f"Soldier ID: {soldier_id}")
+        st.write(f"Name: {name}")
+        st.write(f"Surname: {surname}")
+        st.write(f"Height: {height}")
+        st.write("---")  # Separator
