66374429f670d21741f761dde14c5b61fed72d9a
lordtemplar <lordtemplar0@gmail.com>
2023-10-29 12:40:06 +0700

    Update website.py

--- website.py
+++ website.py
@@ -8,10 +8,13 @@ creds = ServiceAccountCredentials.from_json_keyfile_name("soldier-risk-calculato
 client = gspread.authorize(creds)
 
 # Streamlit UI
-st.title("Google Sheets Data Fetcher")
+st.title("Google Sheets Data Fetcher by Soldier ID")
 
 # Fixed Google Sheet URL
 SHEET_URL = "https://docs.google.com/spreadsheets/d/1sTGeISgyGZgngAkBl86cPcdIzfYKFyotXQUhcslGilw/edit#gid=1050462434"
+
+# Input for Soldier ID
+soldier_id = st.text_input("Enter Soldier ID:")
 
 # Fetch and display data using the specified structure
 if st.button("Fetch Data"):
@@ -21,17 +24,22 @@ if st.button("Fetch Data"):
     # Get all records
     records = worksheet.get_all_records()
     
-    for record in records:
-        timestamp = record.get("Timestamp", "N/A")
-        soldier_id = record.get("Soldier ID", "N/A")
-        name = record.get("Name", "N/A")
-        surname = record.get("Surname", "N/A")
-        height = record.get("Height", "N/A")
-        
-        # Display the data in Streamlit
-        st.write(f"Timestamp: {timestamp}")
-        st.write(f"Soldier ID: {soldier_id}")
-        st.write(f"Name: {name}")
-        st.write(f"Surname: {surname}")
-        st.write(f"Height: {height}")
-        st.write("---")  # Separator
+    # Filter records by Soldier ID
+    matching_records = [record for record in records if record.get("Soldier ID") == soldier_id]
+    
+    if matching_records:
+        for record in matching_records:
+            timestamp = record.get("Timestamp", "N/A")
+            name = record.get("Name", "N/A")
+            surname = record.get("Surname", "N/A")
+            height = record.get("Height", "N/A")
+            
+            # Display the data in Streamlit
+            st.write(f"Timestamp: {timestamp}")
+            st.write(f"Soldier ID: {soldier_id}")
+            st.write(f"Name: {name}")
+            st.write(f"Surname: {surname}")
+            st.write(f"Height: {height}")
+            st.write("---")  # Separator
+    else:
+        st.warning(f"No data found for Soldier ID: {soldier_id}")
