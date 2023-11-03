d51800d71133f9c9375672b1f8145788b9709069
lordtemplar <lordtemplar0@gmail.com>
2023-11-03 20:37:33 +0700

    Update website.py

--- website.py
+++ website.py
@@ -14,6 +14,18 @@ def send_line_notification(token, message):
     response = requests.post(url, headers=headers, data=data)
     return response.status_code
 
+def append_to_new_sheet(soldier_id, name, surname, height, weight, body_temperature, body_water, new_weight, urine_color, bmi_risk, body_temperature_risk, body_water_risk, urine_color_risk):
+    NEW_SHEET_URL = "https://docs.google.com/spreadsheets/d/12p8ohDnd5ZyO9H6nU6bjlND0_1pyE-cPguhcewape88/edit?usp=sharing"
+    spreadsheet = client.open_by_url(NEW_SHEET_URL)
+    worksheet = spreadsheet.sheet1
+    
+    # Get the current timestamp
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    # Append the data along with the timestamp
+    data = [timestamp, soldier_id, name, surname, height, weight, body_temperature, body_water, new_weight, urine_color, bmi_risk, body_temperature_risk, body_water_risk, urine_color_risk]
+    worksheet.append_row(data)
+
 # Setup gspread
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("soldier-risk-calculator-93be17dccbd3.json", scope)
@@ -77,8 +89,8 @@ if st.session_state.fetched:
     if st.button("คำนวนความเสี่ยง"):
         height = float(st.session_state.record.get("Height", 1))
         bmi = new_weight / ((height / 100) * (height / 100))
-        bmi_risk = "RED" if bmi > 30 else "ORANGE" if 25 < bmi < 30 else "GREEN"
-        body_temperature_risk = "RED" if body_temperature > 37.2 else "YELLOW" if 36.8 < body_temperature <= 37.2 else "GREEN"
+        bmi_risk = "RED" if bmi > 30 else "ORANGE" if 25 <= bmi < 30 else "GREEN"
+        body_temperature_risk = "RED" if body_temperature > 37.2 else "YELLOW" if 36.8 <= body_temperature <= 37.2 else "GREEN"
         body_water_risk = "RED" if body_water < 50 else "YELLOW" if 50 <= body_water < 55 else "GREEN"
         urine_color_risk = ["GREEN", "GREEN", "YELLOW", "ORANGE", "RED"][urine_color]
     
@@ -97,36 +109,25 @@ if st.session_state.fetched:
         st.markdown(f"ความเสี่ยงจาก ปริมาณน้ำในร่างกาย: <div style='display: inline-block; width: {box_size}; height: {box_size}; background-color: {color_mapping[body_water_risk]}'></div>", unsafe_allow_html=True)
         st.markdown(f"ความเสี่ยงจาก สีปัสสาวะ: <div style='display: inline-block; width: {box_size}; height: {box_size}; background-color: {color_mapping[urine_color_risk]}'></div>", unsafe_allow_html=True)
 
-        # Emoji mapping for risks
-        emoji_mapping = {
-            "RED": "🟥",
-            "ORANGE": "🟧",
-            "YELLOW": "🟨",
-            "GREEN": "🟩"
-        }
-        
-        # Format the message with emojis
-        # Check if any of the risks are YELLOW, ORANGE, or RED
-        if any(risk in ["YELLOW", "ORANGE", "RED"] for risk in [bmi_risk, body_temperature_risk, body_water_risk, urine_color_risk]):
-            # Format the message with emojis
-            message = f"""
-            
---- ข้อมูลทหารใหม่ ---
-รหัสประจำตัว: {soldier_id}
-ชื่อ: {st.session_state.record.get('Name', 'N/A')} {st.session_state.record.get('Surname', 'N/A')}
-
------ ความเสี่ยง -----
-BMI: {emoji_mapping[bmi_risk]}
-อุณภูมิร่างกาย: {emoji_mapping[body_temperature_risk]}
-ปริมาณน้ำในร่างกาย: {emoji_mapping[body_water_risk]}
-สีปัสสาวะ: {emoji_mapping[urine_color_risk]}"""
-    
-            # Send the notification
-            token = "S0zdZC7JLAu6l5vnHFublLHgeK3htiNWizef2aw6a4D"  # Your LINE Notify token
-            send_line_notification(token, message)
+        # Append the new data to the new sheet
+        append_to_new_sheet(
+            soldier_id,
+            st.session_state.record.get('Name', 'N/A'),
+            st.session_state.record.get('Surname', 'N/A'),
+            st.session_state.record.get('Height', 'N/A'),
+            st.session_state.record.get('Weight', 'N/A'),
+            body_temperature,
+            body_water,
+            new_weight,
+            urine_color,
+            bmi_risk,
+            body_temperature_risk,
+            body_water_risk,
+            urine_color_risk
+        )
     
     # Reset button
     if st.button("รีเซ็ต"):
         st.session_state.fetched = False
         st.session_state.record = {}
-        st.experimental_rerun()  # Refresh the page 
+        st.experimental_rerun()  # Refresh the page
